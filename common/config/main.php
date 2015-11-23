68a4e40eef80224061c3d5aac69e350031946087
zhanglu <1216160989@qq.com>
2015-11-23 13:46:34 +0800

    hehe

--- common/config/main.php
+++ common/config/main.php
@@ -6,20 +6,5 @@ return [
             'class' => 'yii\caching\FileCache',
         ],
 
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=119.29.101.11;dbname=mysql',
-            'username' => 'root',
-            'password' => 'zhanglu',
-            // 'tablePrefix' => 'wly_',
-            'charset' => 'utf8',
-
-            // 'class' => 'yii\db\Connection',
-            // 'dsn' => 'mysql:host=localhost;dbname=niutou-local',
-            // 'username' => 'root',
-            // 'password' => '',
-            // 'tablePrefix' => 'wly_',
-            // 'charset' => 'utf8',
-        ],
     ],
 ];
